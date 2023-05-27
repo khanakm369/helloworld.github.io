72f983aa0d2fa62ba24a4f7512deffe46b66fde8
KHANAK M <123806581+khanakm369@users.noreply.github.com>
2023-05-27 13:53:40 +0000

    Basic_Js

---
+++ 01_basics.js
@@ -0,0 +1,21 @@
+//console.log("Hello World")
+
+const accountId = 145553
+
+let accountEmail = "khanakm369@gmail.com"
+
+var accountPassword = "123456"
+
+accountCity = "Bangalore"
+
+console.log(accountId)
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
+
+/* Never in JavaScript use Var*/
+
+console.log(3 + 3)
+
+console.log( typeof accountId)
+console.log( typeof undefined); // undefined
+console.log(typeof null); //object in js its also called javascript error
